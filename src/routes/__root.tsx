a93406e291afaad0cdc63902fe7f4b876a734d24
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-02-20 20:23:31 +0200

    feat: integrate TanStack Router and update project structure

---
+++ src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+const RootLayout = () => (
+  <>
+    <div className='p-2 flex gap-2'>
+      <Link to='/about'>About</Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
