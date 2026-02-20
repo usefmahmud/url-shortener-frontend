af81a753d48f703d37b13aa681603633d073ebff
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-02-20 20:27:18 +0200

    feat: add Tailwind CSS support and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
-      target: "react",
+      target: 'react',
       autoCodeSplitting: true,
     }),
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
 });
